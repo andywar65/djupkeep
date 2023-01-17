0f075af9c9dcfc19c7534928b432f796bc61d3c5
andywar65 <andy.war1965@gmail.com>
2023-01-18 00:58:40 +0100

    min zoom set to -2

--- static/djupkeep/js/origin.js
+++ static/djupkeep/js/origin.js
@@ -1,9 +1,10 @@
 window.addEventListener("map:init", function (event) {
     var map = event.detail.map; // Get reference to map
     map.options.crs = L.CRS.Simple;
+    map.options.minZoom = -2
     let image_path = JSON.parse(document.getElementById("image_path").textContent);
     let image_size = JSON.parse(document.getElementById("image_size").textContent);
-    let bounds = [[0,0], image_size];
-    let image = L.imageOverlay(image_path, bounds).addTo(map);
+    const bounds = [[0,0], image_size];
+    const image = L.imageOverlay(image_path, bounds).addTo(map);
     map.fitBounds(bounds);
 });
