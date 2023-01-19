b959475ca16f23e9f2c64779bbff6be3a5575879
andywar65 <andy.war1965@gmail.com>
2023-01-19 14:21:06 +0100

    trim unit lines at image

--- static/djupkeep/js/project_update.js
+++ static/djupkeep/js/project_update.js
@@ -27,17 +27,17 @@ window.addEventListener("map:init", function (event) {
     // Add units
     if (unit !== "" ) {
         let length = Math.sqrt(Math.pow(unit[0], 2) + Math.pow(unit[1], 2))
-        for (let i = 1; i < 180/length; i++) {
-            L.polyline([[0, i*length], [90, i*length]], {color: "red", weight: 1}).addTo(map);
+        for (let i = 1; i < (image_size[0]-origin[0])/length; i++) {
+            L.polyline([[0, i*length], [image_size[1]-origin[1], i*length]], {color: "red", weight: 1}).addTo(map);
         }
-        for (let i = 1; i < 90/length; i++) {
-            L.polyline([[i*length, 0], [i*length, 180]], {color: "red", weight: 1}).addTo(map);
+        for (let i = 1; i < (image_size[1]-origin[1])/length; i++) {
+            L.polyline([[i*length, 0], [i*length, image_size[0]-origin[0]]], {color: "red", weight: 1}).addTo(map);
         }
     }
     // Add axis
     if (origin !== "" ) {
-        L.polyline([[0, 0], [90, 0]]).addTo(map);
-        L.polyline([[0, 0], [0, 180]]).addTo(map);
+        L.polyline([[0, 0], [image_size[1]-origin[1], 0]]).addTo(map);
+        L.polyline([[0, 0], [0, image_size[0]-origin[0]]]).addTo(map);
     }
     map.fitBounds(bounds);
 });
