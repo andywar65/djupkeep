f3615d76bf33e80c2c5e07fd914ae86bed6a4a45
andywar65 <andy.war1965@gmail.com>
2023-01-19 18:00:49 +0100

    adding popups to lines

--- static/djupkeep/js/project_update.js
+++ static/djupkeep/js/project_update.js
@@ -8,6 +8,7 @@ window.addEventListener("map:init", function (event) {
     // Set new CRS
     map.options.crs = L.CRS.Simple;
     map.options.zoomSnap = 0.1;
+    L.control.scale().remove();
     // Get image attributes
     const image_path = JSON.parse(document.getElementById("image_path").textContent);
     const image_size = JSON.parse(document.getElementById("image_size").textContent);
@@ -30,16 +31,16 @@ window.addEventListener("map:init", function (event) {
         let xtrim = parseInt((image_size[1]-origin[1])/length)*length
         let ytrim = parseInt((image_size[0]-origin[0])/length)*length
         for (let i = 1; i < (image_size[0]-origin[0])/length; i++) {
-            L.polyline([[0, i*length], [xtrim, i*length]], {weight: 1}).addTo(map);
+            L.polyline([[0, i*length], [xtrim, i*length]], {weight: 1}).bindPopup("Y="+i.toString()).addTo(map);
         }
         for (let i = 1; i < (image_size[1]-origin[1])/length; i++) {
-            L.polyline([[i*length, 0], [i*length, ytrim]], {weight: 1}).addTo(map);
+            L.polyline([[i*length, 0], [i*length, ytrim]], {weight: 1}).bindPopup("X="+i.toString()).addTo(map);
         }
     }
     // Add axis
     if (origin !== "" ) {
-        L.polyline([[0, 0], [90, 0]], {color: "red"}).addTo(map);
-        L.polyline([[0, 0], [0, 180]], {color: "red"}).addTo(map);
+        L.polyline([[0, 0], [90, 0]], {color: "red"}).bindPopup("Y=0").addTo(map);
+        L.polyline([[0, 0], [0, 180]], {color: "red"}).bindPopup("X=0").addTo(map);
     }
     map.fitBounds(bounds);
 });
