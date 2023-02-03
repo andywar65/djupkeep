9110fade0141b48c4be86197f9df383fe61a9f74
andywar65 <andy.war1965@gmail.com>
2023-02-03 15:13:50 +0100

    loading correct map

--- static/djupkeep/js/location_detail.js
+++ static/djupkeep/js/location_detail.js
@@ -14,23 +14,52 @@ function map_init(map, options) {
       }
     }
 
-    const base_map = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-      {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        maxZoom: 19,
-      });
-
-    const mapbox_token = JSON.parse(document.getElementById("mapbox_token").textContent);
-
-    const sat_map = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-      {
-        attribution: 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 19,
-        tileSize: 512,
-        zoomOffset: -1,
-        id: "mapbox/satellite-v9",
-        accessToken: mapbox_token
-      });
+    // Delete all default layers
+    map.eachLayer(function (layer) {
+        map.removeLayer(layer);
+    });
+    // Set new CRS
+    map.options.crs = L.CRS.Simple;
+    map.options.zoomSnap = 0.1;
+    // Get image attributes
+    const image_path = JSON.parse(document.getElementById("image_path").textContent);
+    const image_size = JSON.parse(document.getElementById("image_size").textContent);
+    const origin = JSON.parse(document.getElementById("origin_coords").textContent);
+    const unit = JSON.parse(document.getElementById("unit_coords").textContent);
+    const meters = JSON.parse(document.getElementById("length_meters").textContent);
+    // Get drawing bounds
+    function get_bounds(image_size, origin) {
+        if (origin !== "" ) {
+            return [[-origin[1], -origin[0]], [image_size[1]-origin[1], image_size[0]-origin[0]]];
+        } else {
+            return [[0, 0], [image_size[1], image_size[0]]];
+        }
+    };
+    const bounds = get_bounds(image_size, origin);
+    // Add image to map
+    L.imageOverlay(image_path, bounds).addTo(map);
+    // Add units
+    if (unit !== "" ) {
+        let length = Math.sqrt(Math.pow(unit[0], 2) + Math.pow(unit[1], 2))
+        for (let i = 1; i < (image_size[0]-origin[0])/length; i++) {
+            L.polyline([[-origin[1], i*length], [(image_size[1]-origin[1]), i*length]], {weight: 1}).bindPopup("X=m "+(i*meters).toString()).addTo(map);
+        }
+        for (let i = -1; i > (-origin[0])/length; i--) {
+            L.polyline([[-origin[1], i*length], [(image_size[1]-origin[1]), i*length]], {weight: 1}).bindPopup("X=m "+(i*meters).toString()).addTo(map);
+        }
+        for (let i = 1; i < (image_size[1]-origin[1])/length; i++) {
+            L.polyline([[i*length, -origin[0]], [i*length, (image_size[0]-origin[0])]], {weight: 1}).bindPopup("Y=m "+(i*meters).toString()).addTo(map);
+        }
+        for (let i = -1; i > (-origin[1])/length; i--) {
+            L.polyline([[i*length, -origin[0]], [i*length, (image_size[0]-origin[0])]], {weight: 1}).bindPopup("Y=m "+(i*meters).toString()).addTo(map);
+        }
+    }
+    // Add axis
+    if (origin !== "" ) {
+        L.polyline([[-origin[1], 0], [(image_size[1]-origin[1]), 0]], {color: "red"}).bindPopup("X=0").addTo(map);
+        L.polyline([[0, -origin[0]], [0, (image_size[0]-origin[0])]], {color: "red"}).bindPopup("Y=0").addTo(map);
+    }
+    map.fitBounds(bounds);
 
     const layer_control = L.control.layers(null).addTo(map);
 
@@ -83,7 +112,7 @@ function map_init(map, options) {
       }
     }
 
-    getCollections()
+    // getCollections()
 
     addEventListener("refreshCollections", function(evt){
       getCollections();
